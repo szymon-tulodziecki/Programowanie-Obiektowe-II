8c3bc10d3006abdb4a1e786315ef3783b73c8c68
Szymon Tułodziecki <21312@student.ans-elblag.pl>
2025-04-07 15:09:59 +0200

    Refactor RSA decryption method to print decrypted message and update main method for clarity

---
+++ lab_6/Zad_2/Test.java
@@ -0,0 +1,11 @@
+package Zad_2;
+
+import java.util.Date;
+class Test {
+public static void main(String[ ] param) throws InterruptedException {
+System.out.println(new Date(1743782373503L));
+System.out.println(new Date(1743782373505L));
+System.out.println(new Date(1743782373506L));
+System.out.println(new Date(1743782373507L));
+}
+} 
